463cace702d22e73c6068b33d3920e18a5d5c211
muhallimir <mir23wpurposes@gmail.com>
2025-04-18 17:49:45 +0800

    fix: FOUC initial render

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = async () => {
       {
         source: "/",
         destination: "/store",
-        permanent: true,
+        permanent: false,
       },
     ],
     images: {
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,57 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+	Html,
+	Head,
+	Main,
+	NextScript,
+	DocumentContext,
+	DocumentInitialProps,
+} from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import createEmotionCache from "@helpers/createEmotionCache";
 
-export default function Document() {
-	return (
-		<Html lang="en">
-			<Head />
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+export default class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const originalRenderPage = ctx.renderPage;
+
+		const cache = createEmotionCache();
+		const { extractCriticalToChunks } = createEmotionServer(cache);
+
+		ctx.renderPage = () =>
+			originalRenderPage({
+				enhanceApp: (App: any) => (props) => <App emotionCache={cache} {...props} />,
+			});
+
+		const initialProps = await Document.getInitialProps(ctx);
+		const emotionChunks = extractCriticalToChunks(initialProps.html);
+
+		const emotionStyleTags = emotionChunks.styles.map(
+			(style: { key: string; ids: string[]; css: string }) => (
+				<style
+					data-emotion={`${style.key} ${style.ids.join(" ")}`}
+					key={style.key}
+					dangerouslySetInnerHTML={{ __html: style.css }}
+				/>
+			)
+		);
+
+		return {
+			...initialProps,
+			styles: [
+				...(Array.isArray(initialProps.styles) ? initialProps.styles : [initialProps.styles]),
+				...emotionStyleTags,
+			],
+		};
+	}
+
+	render() {
+		return (
+			<Html lang="en">
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
